47a4503fc779ca022761f758abf21e993c2711a2
seanpx <37602629+seanpx@users.noreply.github.com>
2023-12-20 21:35:12 +0800

    2023-12-20 #05 - hit error when push via IDEA, switch to Git Desktop and try

--- src/main/java/com/seanpx/Main.java
+++ src/main/java/com/seanpx/Main.java
@@ -20,7 +20,7 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            x = 2 + i;
+            x = 5 + i;
             System.out.println("x = " + x);
         }
 
