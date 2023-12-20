eb0165d5a3e0c839e581fcaf5cb49d151f720712
seanpx <seanpx@gmail.com>
2023-12-20 21:13:50 +0800

    2023-12-20 #04 - Main.java - introduced SpringBootApplication annotation, and successfully invoke the Main class.

--- src/main/java/com/seanpx/Main.java
+++ src/main/java/com/seanpx/Main.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
@@ -22,5 +23,7 @@ public class Main {
             x = 2 + i;
             System.out.println("x = " + x);
         }
+
+        SpringApplication.run(Main.class, args);
     }
 }
