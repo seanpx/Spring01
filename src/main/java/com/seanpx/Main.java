fc2eaf5074d30de04ddeb0f38be6ae3e66429eea
seanpx <seanpx@gmail.com>
2023-12-20 20:35:02 +0800

    2023-12-20 #02 - revised pom.xml by referencing spring initilizer web generated pom.xml

--- src/main/java/com/seanpx/Main.java
+++ src/main/java/com/seanpx/Main.java
@@ -14,12 +14,12 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        int x = 1;
+        System.out.println("Hello and welcome!");
+        int x;
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            x = 1 + i;
+            x = 2 + i;
             System.out.println("x = " + x);
         }
     }
