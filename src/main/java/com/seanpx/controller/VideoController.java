da47811d04110cc241926793561fc39990daebf3
seanpx <37602629+seanpx@users.noreply.github.com>
2023-12-22 09:40:00 +0800

    2023-12-22 #01 - use LinkedHashMap instead of HashMap to keep the order of the JSON Response

--- src/main/java/com/seanpx/controller/VideoController.java
+++ src/main/java/com/seanpx/controller/VideoController.java
@@ -3,8 +3,7 @@ package com.seanpx.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 //@Controller - it doesn't return JSON
 @RestController
@@ -13,9 +12,12 @@ public class VideoController {
 
     @RequestMapping("list")
     public Object list(){
-        Map<String,String> map = new HashMap<>();
+        Map<String,String> map = new LinkedHashMap<>();
         map.put("key-1","video card 1");
+
         map.put("key-2","video card 2");
+
+        map.put("key-3","video card 3");
 
         return map;
     }
